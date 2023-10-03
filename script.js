7d77f139db4908b4f00b9e2566f0a128b6c58f3a
Krzysztof Majerczyk-Marchewka <skanerooo5@gmail.com>
2023-10-03 23:41:24 +0200

    added question 7 and made script.js more responsive

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const correctAnswer = ["D", "B", "C", "B", "D", "A"];
+const correctAnswer = ["D", "B", "C", "B", "D", "A", "B"];
 const form = document.querySelector(".quiz-form");
 const result = document.querySelector(".result");
 const questions=document.querySelectorAll(".question");
@@ -7,7 +7,7 @@ form.addEventListener("submit", event => {
     event.preventDefault();
 
     let score=0;
-    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value];
+    const userAnswers = [...document.querySelectorAll('input[type=radio]:checked')].map(el => el.value);
 
     userAnswers.forEach((answer, index) => {
         if(answer === correctAnswer[index]){
@@ -20,5 +20,5 @@ form.addEventListener("submit", event => {
     
     scrollTo(0,0);
     result.classList.remove("hide");
-    result.querySelector("p").textContent = `You scored ${score}/6!`;
+    result.querySelector("p").textContent = `You scored ${score}/${userAnswers.length}!`;
 });
