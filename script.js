03643cf6570a22cd45ad9ee83efa0e70561dc410
Avionic Harshit <78672319+avionicharshit-byte@users.noreply.github.com>
2023-10-04 10:29:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ form.addEventListener("submit", event => {
     event.preventDefault();
 
     let score=0;
-    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q5.value];
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q7.value];
 
     userAnswers.forEach((answer, index) => {
         if(answer === correctAnswer[index]){
