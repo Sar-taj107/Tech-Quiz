0fc57f13c6fd6181d0069b1ae4b9f01236cb25c1
trdxlucifer <singhdepu566@gmail.com>
2023-10-04 08:51:11 +0530

    added question and updated script.js

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-const correctAnswer = ["D", "B", "C", "B", "D", "A"];
+const correctAnswer = ["D", "B", "C", "B", "D", "A", "A"];
 const form = document.querySelector(".quiz-form");
 const result = document.querySelector(".result");
-const questions=document.querySelectorAll(".question");
+const questions = document.querySelectorAll(".question");
 
-form.addEventListener("submit", event => {
-    event.preventDefault();
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-    let score=0;
-    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value];
+  let score = 0;
+  const userAnswers = [
+    form.q1.value,
+    form.q2.value,
+    form.q3.value,
+    form.q4.value,
+    form.q5.value,
+    form.q6.value,
+    form.q7.value,
+  ];
 
-    userAnswers.forEach((answer, index) => {
-        if(answer === correctAnswer[index]){
-        score += 1;
-        questions[index].classList.add("correct"); 
-        } else {
-        questions[index].classList.add("wrong");
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswer[index]) {
+      score += 1;
+      questions[index].classList.add("correct");
+    } else {
+      questions[index].classList.add("wrong");
     }
-    });
-    
-    scrollTo(0,0);
-    result.classList.remove("hide");
-    result.querySelector("p").textContent = `You scored ${score}/6!`;
+  });
+
+  scrollTo(0, 0);
+  result.classList.remove("hide");
+  result.querySelector("p").textContent = `You scored ${score}/6!`;
 });
