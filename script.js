df6e8a0cf080a843ed33922218cf6378ca70d81d
Sar-taj107 <hussainarman065@gmail.com>
2023-10-05 19:18:45 +0530

    update

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
 
-const correctAnswer = ["D", "B", "C", "B", "D", "A", "B","D","A","B"];
+const correctAnswer = ["D", "B", "C", "B", "D", "A", "B", "D", "A", "B"];
+const correctOptions = ["D", "B", "C", "B", "D", "A", "B", "D", "A", "B"];
 const form = document.querySelector(".quiz-form");
 const result = document.querySelector(".result");
-const questions=document.querySelectorAll(".question");
+const questions = document.querySelectorAll(".question");
 
-form.addEventListener("submit", event => {
-    event.preventDefault();
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-    let score=0;
-    const userAnswers = [...document.querySelectorAll('input[type=radio]:checked')].map(el => el.value);
+  let score = 0;
+  const userAnswers = [
+    ...document.querySelectorAll("input[type=radio]:checked"),
+  ].map((el) => el.value);
 
-    userAnswers.forEach((answer, index) => {
-        if(answer === correctAnswer[index]){
-        score += 1;
-        questions[index].classList.add("correct"); 
-        } else {
-        questions[index].classList.add("wrong");
-    }
-    });
     
-    scrollTo(0,0);
-    result.classList.remove("hide");
-    result.querySelector("p").textContent = `You scored ${score}/${userAnswers.length}!`;
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswer[index]) {
+      score += 1;
+      questions[index].classList.add("correct");
+    } else {
+      questions[index].classList.add("wrong");
+
+    
+      // Find the correct option and highlight it
+      const options = questions[index].querySelectorAll(".option");
+      options.forEach((option) => {
+        const optionValue = option.querySelector("input").value;
+        if (correctOptions[index] === optionValue) {
+          option.classList.add("correct-option");
+        }
+      });
+    }
+    
+  });
+
+  scrollTo(0, 0);
+  result.classList.remove("hide");
+  result.querySelector(
+    "p"
+  ).textContent = `You scored ${score}/${userAnswers.length}!`;
 });
