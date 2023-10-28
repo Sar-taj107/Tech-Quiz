42106311977cb1289160bbb168a35e8fb579fa4a
Maaz Ahmed <ahmedmaaz73@gmail.com>
2023-10-29 02:47:41 +0530

    Added scroll to top button

--- script.js
+++ script.js
@@ -48,3 +48,19 @@ if (score === userAnswers.length) {
   messageElement.textContent = "Try Once more!";
 }
 });
+
+let mybutton = document.getElementById("scrolltoTop");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
+}
